75ceccf87768dfd93a81e5b175f24ef3d787ec88
jugal09xx <65349424+jugal09xx@users.noreply.github.com>
2021-09-16 20:27:29 +0530

    Initial commit

---
+++ src/components/container/Canvas.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Canvas() {
+  return (
+    <div>
+      <h4>this is the canvas component</h4>
+    </div>
+  )
+}
+
+export default Canvas
---
+++ src/components/container/main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Canvas from './Canvas';
+
+function Main() {
+  return (
+    <div>
+      <h1>Hello from Container</h1>
+      <Canvas/>      
+    </div>
+  )
+}
+
+export default Main
